e727b05dbf4f8ddc942b5b2bd77993baea0b1e19
Johan Palomino <johan.palomino@edelman.com>
2025-04-02 19:06:36 +0200

    Use copilot suggestions

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -7,9 +7,12 @@ return {
         auto_trigger = true,
         hide_during_completion = vim.g.ai_cmp,
         keymap = {
-          accept = false, -- handled by nvim-cmp / blink.cmp
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
           next = "<M-]>",
           prev = "<M-[>",
+          dismiss = "<C-]>",
         },
       },
       panel = { enabled = false },
