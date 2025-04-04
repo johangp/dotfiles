f461a3a6bf6d45077a0ba87a9231a0e2def7d9e8
Johan Palomino <johan.palomino@edelman.com>
2025-04-04 11:57:35 +0200

    use basedpyright

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "pyright", "jsonls" }
+local servers = { "html", "cssls", "jsonls", "basedpyright", "ruff" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
