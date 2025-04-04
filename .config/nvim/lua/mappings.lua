71d4edb2a5a1d0f479f4277c683829cb5a4536eb
Johan Palomino <johan.palomino@edelman.com>
2025-04-04 12:34:36 +0200

    Add persistence

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -17,4 +17,12 @@ map("n", "<leader>to", function() neotest.output.open({ enter = true }) end, { d
 map("n", "<leader>tw", function() neotest.watch.toggle(vim.fn.expand("%")) end, { desc = "Toggle test watch" })
 map("n", "<leader>tS", function() neotest.run.stop() end, { desc = "Stop running test" })
 
+
+-- persistence
+map("n", "<leader>qs", function() require("persistence").load() end, { desc = "Restore the sessions from current" })
+map("n", "<leader>qS", function() require("persistence").select() end, { desc = "Select session" })
+map("n", "<leader>ql", function() require("persistence").load({ last = true }) end, {desc = "Restore the last session" })
+map("n", "<leader>qd", function() require("persistence").stop() end, { desc = "Stop current session" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
