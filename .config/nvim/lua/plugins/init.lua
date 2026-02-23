99a456e98942f16de0545129a7ac0d685290d248
Johan Palomino <johan.palomino@edelman.com>
2026-02-23 22:40:28 +0100

    chore: remove copilot and avante from neovim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -54,7 +54,6 @@ return {
         { "Bilal2453/luvit-meta", lazy = true },
       },
       library = {
-        { path = "~/projects/avante.nvim/lua", words = { "avante" } },
         { path = "luvit-meta/library", words = { "vim%.uv" } },
       },
     },
